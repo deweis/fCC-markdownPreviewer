6fc25519b26622473a4b4554b73ec9d3481692c4
Dennis <deweis@gmx.ch>
2018-05-16 14:10:25 +0200

    Config browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 // compile
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
 
-// compile and watch
-gulp.task('sass:watch', function () {
+// Watch and Serve
+gulp.task('serve', ['sass'], function () {
+  browserSync.init({
+    server: './',
+  });
+
   gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./*.html').on('change', browserSync.reload);
 });
+
+// Default
+gulp.task('default', ['serve']);
